566e6430852c8dddb0d678604c1a228f687c25e6
Subham Das <Subhamd@emids.com>
2023-01-23 12:21:30 +0530

    Added changes in jobs ui

--- Marketplace-React/src/App.js
+++ Marketplace-React/src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-
-import Home from './components/home/home.jsx'
-import Header from './components/header/header.jsx'
 import {
   BrowserRouter as Router,
   Routes,
   Route
 } from "react-router-dom"
 import Dashboard from './pages/Dashboard/dashboard.jsx';
+import Home from './components/home/home.jsx';
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-         
+        <Route path="/home" element={<Home/>}/>
           <Route path="/" element={<Dashboard/>}/>
         </Routes>
       </Router>
--- Marketplace-React/src/pages/Dashboard/dashboard.jsx
+++ Marketplace-React/src/pages/Dashboard/dashboard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../components/header/header";
 import Home from "../../components/home/home.jsx";
+
 function Dashboard() {
   return <div>
     <div>
