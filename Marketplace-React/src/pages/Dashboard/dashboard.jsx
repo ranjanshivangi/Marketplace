f5a0bfcc5f9a4a1d8678f07e25f8941b594f6e59
Shivangi Ranjan <shivangiec1098@gmail.com>
2023-01-23 11:20:16 +0530

    Added JD Page UI

---
+++ Marketplace-React/src/pages/Dashboard/dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../components/header/header";
+import Home from "../../components/home/home.jsx";
+function Dashboard() {
+  return <div>
+    <div>
+    <Header/>
+    <Home />
+    </div>
+  </div>;
+}
+
+export default Dashboard;
