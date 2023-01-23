cee105c94df89f3b22f1e5e5ae8b6987e3413b1f
Shivangi Ranjan <shivangiec1098@gmail.com>
2023-01-23 11:45:22 +0530

    Added Header to the JD page

--- Marketplace-React/src/pages/jobdescription/jobdescription.jsx
+++ Marketplace-React/src/pages/jobdescription/jobdescription.jsx
@@ -2,16 +2,15 @@ import React from "react";
 import '../jobdescription/jobdescription.scss';
 import RRDetails from "../../components/rrdetails/rrdetails";
 import JDDetails from "../../components/jddetsails/jddetails";
-
-
+import Header from "../../components/header/header";
 
 const JobDescription = () => {
-return (
-    <div className="container">    
-    <RRDetails/>
-    <JDDetails/>
-    </div>
-    
-)
+    return (
+        <div className="container">
+            <Header />
+            <RRDetails />
+            <JDDetails />
+        </div>
+    )
 }
 export default JobDescription;
