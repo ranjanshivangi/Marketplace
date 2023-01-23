acbacc2f23a6e91cc9772d2e973dd066900da0a8
Shivangi Ranjan <shivangiec1098@gmail.com>
2023-01-23 10:43:04 +0530

    Added JD Page UI

--- Marketplace-React/src/App.js
+++ Marketplace-React/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
-import Home from './pages/home/home.jsx'
+import Home from './pages/home/home.jsx';
+import JobDescription from './pages/jobdescription/jobdescription.jsx';
+
 import {
   BrowserRouter as Router,
   Routes,
@@ -13,6 +15,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home/>}></Route>
+          <Route path="/view" element={<JobDescription/>}></Route>
         </Routes>
       </Router>
     </div>
