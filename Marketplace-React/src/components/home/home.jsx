2701db118b1b4cf65e7cc33f920996cee556315a
Shivangi Ranjan <shivangiec1098@gmail.com>
2023-01-24 13:30:08 +0530

    Added changes to css of home page

--- Marketplace-React/src/components/home/home.jsx
+++ Marketplace-React/src/components/home/home.jsx
@@ -9,45 +9,52 @@ import SettingsIcon from '@mui/icons-material/Settings';
 import AssignmentTurnedInOutlinedIcon from '@mui/icons-material/AssignmentTurnedInOutlined';
 
 import Card from '@mui/material/Card';
+import { CardContent } from "@mui/material";
 
 const Home = () => {
   return (
     <div className="jobpage">
       <div className="jobs">
         <Card className="menu">
-          <div className="iconcss">
-            <BookmarkOutlinedIcon color="action" fontSize="large" />
-            <div className="icontext">My Jobs</div>
-          </div >
-          <div className="iconcss" >
-            <NotificationsIcon fontSize="large" color="action" />
-            <div className="icontext">Job Allert</div>
-          </div >
-          <div className="iconcss">
-            <AssignmentTurnedInOutlinedIcon fontSize="large" color="action" />
-            <div className="icontext">Skill Assessments</div>
-          </div>
-          <div className="iconcss">
-            <FeedIcon fontSize="large" color="action" />
-            <div className="icontext">Interview Prep</div>
-          </div>
-          <div className="iconcss">
-            <SettingsIcon fontSize="large" color="action" />
-            <div className="icontext">Application Settings</div>
-          </div>
+          <CardContent>
+            <div className="iconcss">
+              <BookmarkOutlinedIcon color="action" fontSize="large" />
+              <div className="icontext">My Jobs</div>
+            </div >
+            <div className="iconcss" >
+              <NotificationsIcon fontSize="large" color="action" />
+              <div className="icontext">Job Allert</div>
+            </div >
+            <div className="iconcss">
+              <AssignmentTurnedInOutlinedIcon fontSize="large" color="action" />
+              <div className="icontext">Skill Assessments</div>
+            </div>
+            <div className="iconcss">
+              <FeedIcon fontSize="large" color="action" />
+              <div className="icontext">Interview Prep</div>
+            </div>
+            <div className="iconcss">
+              <SettingsIcon fontSize="large" color="action" />
+              <div className="icontext">Application Settings</div>
+            </div>
+          </CardContent>
+
         </Card>
         <Card className="jobcontainer">
-          {homejson.map((data) => {            
-            return <div>
-              <Link to={`/view/${data.jdid}`} state={{jobDetails: data}}>
-                <div className="jobtitle"> {data.JobTitle}</div>
-              </Link>
-              <div className="accountName">{data.AccountName}</div>
-              <div className="workLocation">Location :- {data.WorkLocation}</div>
-              <div className="experience">Experience :-{data.Experience} Years+</div>
+          <CardContent>
+            {homejson.map((data) => {
+              return <div>
+                <Link to={`/view/${data.jdid}`} state={{ jobDetails: data }} >
+                  <div className="jobtitle"> {data.JobTitle}</div>
+                </Link>
+                <div className="accountName">{data.AccountName}</div>
+                <div className="workLocation">Location :- {data.WorkLocation}</div>
+                <div className="experience">Experience :-{data.Experience} Years+</div>
 
-            </div>
-          })}
+              </div>
+            })}
+          </CardContent>
+
         </Card>
       </div>
     </div>
--- Marketplace-React/src/components/rrdetails/rrdetails.jsx
+++ Marketplace-React/src/components/rrdetails/rrdetails.jsx
@@ -8,8 +8,8 @@ import Typography from '@mui/material/Typography';
 import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import PeopleIcon from '@mui/icons-material/People';
-import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
-import TrackChangesOutlinedIcon from '@mui/icons-material/TrackChangesOutlined';
+import TrackChangesOutlinedIcon from '@mui/icons-material/TrackChangesOutlined'
+import QuizIcon from '@mui/icons-material/Quiz';
 
 const RRDetails = ({ jobDetails }) => {
     return (
@@ -19,7 +19,7 @@ const RRDetails = ({ jobDetails }) => {
                     {jobDetails.JobTitle}
                 </Typography>
                 <Typography color="text.secondary" variant="p" className="text">
-                    Chargebee . Bangalore, India . 1 day ago . 12 applicants
+                {jobDetails.AccountName} . Bangalore, India . 1 day ago . 12 applicants
                 </Typography>
                 <Typography className="rrfieldsbox">
                     <div className="rrfields">
@@ -28,7 +28,7 @@ const RRDetails = ({ jobDetails }) => {
                     </div>
                     <div className="rrfields">
                         <CalendarMonthIcon style={{ color: 'gray' }} />
-                        <div className="text2">Product Engineering</div>
+                        <div className="text2">26-Sept-2022</div>
                     </div>
                     <div className="rrfields">
                         <PeopleIcon style={{ color: 'gray' }} />
@@ -36,18 +36,18 @@ const RRDetails = ({ jobDetails }) => {
                     </div>
                     <div className="rrfields">
                         <BusinessCenterIcon style={{ color: 'gray' }} />
-                        <div className="text2">Exp: 4+</div>
+                        <div className="text2">Exp: {jobDetails.Experience}+</div>
                     </div>
                     <div className="rrfields">
                         <PeopleIcon style={{ color: 'gray' }} />
-                        <div className="text2">26-Sept-2022</div>
+                        <div className="text2">Product Engineering</div>
                     </div>
                     <div className="rrfields">
-                        <LightModeOutlinedIcon style={{ color: 'gray' }} />
+                        <QuizIcon style={{ color: 'gray' }} />
                         <div className="text2">Client interview? True</div>
                     </div>
                     <div className="rrfields">
-                        <TrackChangesOutlinedIcon style={{ color: 'gray' }} />
+                        <TrackChangesOutlinedIcon style={{ color: 'green' }} />
                         <div className="text2">OPEN</div>
                     </div>
                 </Typography>
