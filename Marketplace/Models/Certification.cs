d122c59efa19a55031b0a2b680c72a14f3d1ac3c
Shruthin Satheesh <Shruthins@emids.com>
2023-02-08 18:32:10 +0530

    Added Certification,courses and EmployeeHistory model

---
+++ Marketplace/Models/Certification.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Marketplace.Models
+{
+    public class Certification
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CertificationID { get; set; }
+        public string CertificationsName { get; set; }
+        public DateTime CertificationsCompletionDate { get; set; }
+        public string CertificationsFrom { get; set; }
+        public string CertificationsType { get; set; }
+    }
+}
---
+++ Marketplace/Models/Courses.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Marketplace.Models
+{
+    public class Courses
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CourseID { get; set; }
+        public string CourseName { get; set; }
+        public DateTime CourseCompletionDate { get; set; }
+        public string CourseFrom { get; set; }
+        public string CourseType { get; set; }
+    }
+}
