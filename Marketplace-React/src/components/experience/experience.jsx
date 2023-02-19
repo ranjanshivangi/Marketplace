d090ff48769d2f96f77b2b04fc7fb86aa53e0a77
Shivangi Ranjan <shivangiec1098@gmail.com>
2023-02-20 01:19:55 +0530

    Added dynamic "see more" in the UI

--- Marketplace-React/src/components/experience/experience.jsx
+++ Marketplace-React/src/components/experience/experience.jsx
@@ -6,10 +6,10 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
+import ReactReadMoreReadLess from "react-read-more-read-less";
 
 const Experience = ({ companyName, companyData }) => {
-    const [showMore, setShowMore] = React.useState(false);
-
+   
     const uniqueRole = companyData.reduce((acc, cur) => {
         if (!acc.includes(cur.role)) {
             acc.push(cur.role);
@@ -34,12 +34,16 @@ const Experience = ({ companyName, companyData }) => {
                                     <div className="project">
                                         <span className="project-name">{details.projectName}</span>
                                         <span className="date">{details.startDate}-{details.endDate}</span>
-                                        {showMore ? details.description : `${details.description.substring(0, 50)}`}
+                                        <ReactReadMoreReadLess
+                                            charLimit={100}
+                                            readMoreText={"..see more"}
+                                            readLessText={"..see less"}
+                                            readMoreClassName="seemore"
+                                            readLessClassName="seemore"
+                                        >
+                                            {details.description}
+                                        </ReactReadMoreReadLess>
 
-                                        <span className="seemore" onClick={() => setShowMore(!showMore)
-                                        }>
-                                            {showMore ? "...see less" : "...see more"}
-                                        </span>
                                     </div>
                                 ))
                                 } />
