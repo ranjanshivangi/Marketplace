0139a954d63e41281cece829a8b10252ca17e1b6
Subham Das <Subhamd@emids.com>
2023-04-10 13:10:47 +0530

    Added updated scripts for pastemployment and projects

--- Marketplace-Database/Master.Marketplace.PastEmployments.sql
+++ Marketplace-Database/Master.Marketplace.PastEmployments.sql
@@ -1,7 +1,7 @@
 USE [Marketplace]
 GO
 
-/****** Object:  Table [Master].[PastEmployments]    Script Date: 23-03-2023 17:22:11 ******/
+/****** Object:  Table [Master].[PastEmployments]    Script Date: 10-04-2023 13:08:28 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -11,25 +11,18 @@ GO
 CREATE TABLE [Master].[PastEmployments](
 	[EmployeeId] [nvarchar](12) NOT NULL,
 	[XEmployeerID] [int] NOT NULL,
-	[XEmployeerCompanyName] [nvarchar](50) NOT NULL,
 	[XStartDesignation] [nvarchar](50) NOT NULL,
 	[StartDate] [date] NOT NULL,
 	[XLastDesignation] [nvarchar](50) NOT NULL,
 	[EndDate] [date] NOT NULL,
 	[Notes] [nvarchar](1500) NOT NULL,
- CONSTRAINT [PK_PastEmployments] PRIMARY KEY CLUSTERED 
+	[XEMployeerCompanyName] [nvarchar](50) NULL,
+ CONSTRAINT [PK_PastEmployments_1] PRIMARY KEY CLUSTERED 
 (
 	[EmployeeId] ASC,
 	[XEmployeerID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
-GO
-
-ALTER TABLE [Master].[PastEmployments]  WITH CHECK ADD  CONSTRAINT [FK_PastEmployments_Employees] FOREIGN KEY([EmployeeId])
-REFERENCES [Master].[Employees] ([EmployeeId])
-GO
-
-ALTER TABLE [Master].[PastEmployments] CHECK CONSTRAINT [FK_PastEmployments_Employees]
 GO
 
 
